59b94df0adf61e3f2813d341b04c776bf3656738
grazielle-basto <87940829+grazielle-bastos@users.noreply.github.com>
2021-07-26 19:24:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Olá, mundo!");
+console.log("Olá, Grazielle!");
+
+let nome = "Grazielle";
+let idade = 26;
+
+console.log(nome);
+console.log(idade);
+
+nome = "Grazielle Bastos";
+idade = null;
+
+console.log(idade);
+
+// booleanos
+
+const ehMaiorDeIdade = true;
+
+console.log(ehMaiorDeIdade);
